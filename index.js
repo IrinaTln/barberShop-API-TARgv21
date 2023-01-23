890d27e332b10a86af9e51003aa1b1698f92ea76
IrinaTln <ikosenkova@gmail.com>
2023-01-23 13:27:47 +0200

    Update select query

--- index.js
+++ index.js
@@ -19,24 +19,24 @@ const pool = mariadb.createPool({
 
 
 
-app.get("/customers", async (rec, res) =>{
-    let conn
-    try{
-        conn = await pool.getConnection()
-        const rows = await conn.query("SELECT id, CONCAT(firstname, ' ', lastname) AS name from customers")
-        res.send(JSON.stringify(rows))
-    } catch (error) {
-        console.log(error)
-    }finally{
-        conn.end()
-    }
-})
+//app.get("/customers", async (rec, res) =>{
+    //let conn
+    //try{
+       // conn = await pool.getConnection()
+        //const rows = await conn.query("SELECT id, CONCAT(firstname, ' ', lastname) AS name from customers")
+        //res.send(JSON.stringify(rows))
+    //} catch (error) {
+        //console.log(error)
+    //}finally{
+       // conn.end()
+    //}
+//})
 
 app.get("/customers", async (rec, res) =>{
     let conn
     try{
         conn = await pool.getConnection()
-        const rows = await conn.query("SELECT id, firstName, lastName from customers")
+        const rows = await conn.query("SELECT id_customer, firstName, lastName from customers")
         res.send(JSON.stringify(rows))
     } catch (error) {
         console.log(error)
