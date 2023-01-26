3998efb946d307475bdb04b81b5c80971971211b
IrinaTln <ikosenkova@gmail.com>
2023-01-26 21:05:18 +0200

    Try to rewrite query to tabel bookings to recieve customer name, service name and barber name

--- models/Booking.js
+++ models/Booking.js
@@ -1,4 +1,4 @@
-module.exports = (sequelize, Sequelize) => {
+/*module.exports = (sequelize, Sequelize) => {
     const Booking = sequelize.define("booking", {
         id_booking: {
             type: Sequelize.INTEGER,
@@ -25,4 +25,37 @@ module.exports = (sequelize, Sequelize) => {
         
      return Booking
 
+}*/
+
+module.exports = (sequelize, Sequelize) => {
+    const Booking = sequelize.define("booking", {
+        id_booking: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        bookingTime: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        id_customer: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        id_service: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        id_barber: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        }
+    });
+    return Booking;
+
+  Booking.belongsTo(customers, { foreignKey: 'id_customer', as: 'firstName' });
+  Booking.belongsTo(services, { foreignKey: 'id_service', as: 'serviceName' }); 
+  Booking.belongsTo(barbers, { foreignKey: 'id_barber', as: 'barberName' });
 }
+  
+  
