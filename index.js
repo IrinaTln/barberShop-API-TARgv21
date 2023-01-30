bb002bdf011e5426a95356fff3cfbbf6f0ca73d7
IrinaTln <ikosenkova@gmail.com>
2023-01-30 13:42:12 +0200

    Update swagger.yaml

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mariadb = require("mariadb")
 const port = process.env.APP_PORT
 
 const swaggerUi = require("swagger-ui-express")
-const swaggerDocument = require("./docs/swagger.json");
+///const swaggerDocument = require("./docs/swagger.json");
+const yamljs = require("yamljs");
+const swaggerDocument = yamljs.load("./docs/swagger.yaml");
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
