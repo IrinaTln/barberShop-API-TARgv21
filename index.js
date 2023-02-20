4e158870f4248733ba29e12ccd0965a3518e854c
IrinaTln <ikosenkova@gmail.com>
2023-02-20 17:11:24 +0200

    Added cors options for connection backend with frontend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const mariadb = require("mariadb")
 const port = process.env.APP_PORT
 
@@ -10,7 +11,10 @@ const yamljs = require("yamljs");
 const swaggerDocument = yamljs.load("./docs/swagger.yaml");
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json())
-
+const corsOptions = {
+    origin: "https://localhost:5173"
+}
+app.use(cors(corsOptions))
 require("./routes/app_routes")(app)
 
 app.listen(port, async () => {
